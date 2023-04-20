9e8cf9e5df4d8f1357c08cb6f985233720596b93
AbbyIsCoding <abigail_rochelle24@milton.edu>
2023-04-20 15:17:46 -0400

    second commit

--- src/Agents.js
+++ src/Agents.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
+
+
 const Agents = () => {
     
+  
 
         const [Data, setData] = useState({
           name: '',
           role: '',
-          abilities: '', 
+          abilities: '',
           photoUrl: '',
           voiceLine: "",
       
@@ -16,21 +19,30 @@ const Agents = () => {
         useEffect(() => {
           axios.get('https://valorant-api.com/v1/agents')
             .then(res => {
+              var randomNum= Math.floor(Math.random()*res.data.data.length) 
+              console.log(randomNum); 
+              var CharNum = randomNum; 
+              console.log(CharNum);
+              
               console.log('Response from main API: ', res) // All info
               console.log('Valorant Data: ', res.data.data) // just the data 
-              console.log('portraitURL', res.data.data[0].bustPortrait);
-              console.log('voiceLine', res.data.data[0].voiceLine.mediaList[0].wave) 
-              console.log('abilities', res.data.data[0].abilities)
+              // console.log('portraitURL', res.data.data[CharNum].bustPortrait);
+              // console.log('voiceLine', res.data.data[0].voiceLine.mediaList[0].wave) 
+              // console.log('abilities', res.data.data[0].abilities)
+              console.log('voiceLine', res.data.data[CharNum].voiceLine.mediaList[0].wave)
+            
               
-           
+             
+
+
               
               
               setData({ 
-                  name: res.data.data[0].displayName,
-                  role: res.data.data[0].role.displayName,
-                  abilities: '', 
-                  photoUrl: res.data.data[0].bustPortrait,
-                  voiceLine: res.data.data[0].voiceLine.mediaList[0].wave,  
+                  name: res.data.data[CharNum].displayName,
+                  role: res.data.data[CharNum].role.displayName,
+                  abilities: res.data.data[CharNum].abilities[0].displayName,
+                  photoUrl: res.data.data[CharNum].bustPortrait,
+                  voiceLine: res.data.data[CharNum].voiceLine.mediaList[0].wave,  
 
                });
             })
@@ -38,31 +50,22 @@ const Agents = () => {
               console.log(err);
             })
         }, [])
-        let sound = "https://media.valorant-api.com/sounds/464327068.wav";
-       //let sound = Data.voiceLine;
-        console.log('sound', sound); 
-        console.log('from api', Data.voiceLine); 
-        console.log(typeof sound)
-        if(sound === Data.voiceLine)
-        {
-          console.log('yay');
-        }
+      
         return (
 
           <div>
             <h1> Name: {Data.name}</h1>
             <h1> Role: {Data.role}</h1>
             <img src={Data.photoUrl} width = "300" height = "280" alt={Data.name} />
-            {/* <audio controls> 
-              <source src = {Data.voiceLine} type = "audio/wav" />
             
-            </audio> */}
             <audio controls>
-              < source src= {sound}/>
-          {/* <source src= "https://media.valorant-api.com/sounds/464327068.wav" /> */}
+              {Data.voiceLine && (
+                <source src= {Data.voiceLine} />  
+              )}
             </audio>
-      
-            
+
+            <h1>Abilities: {Data.abilities}</h1> 
+          
       
           </div>
         );
