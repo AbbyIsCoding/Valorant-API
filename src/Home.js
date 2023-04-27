84cd24ed45f08b60e16a3d06411c81dfe8c35c2a
AbbyIsCoding <abigail_rochelle24@milton.edu>
2023-04-27 10:24:05 -0400

    final commit

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,15 @@ import './App.css';
 import ValImage from './ValMainImage.jpg';
 
 
-
+// the main page with the little intro 
 function Home(){
     return(
         
         <div>
             <h1> Abby's Freshly Baked Valorant API </h1>
-            <img src={ValImage} width = "400" height = "280" alt='val image' />
+            <img src={ValImage} width = "430" height = "280" alt='val image' />
             <p>Welcome to my website! I have used a published API from one of my favorite video games, Valorant. If you click on the hyper link titled Agents, you will be directed to a random character from the Valorant game! </p> 
-            <p>You'll be able to find the name of each character, their role on the team, their different abilities, and a voiceline!</p>
+            <p>You'll be able to find the name of each character, their role on the team, their different abilities, and a voiceline! To get a new character, just refresh the agents page!</p>
          </div>
 
     )
--- src/pages/Agents.js
+++ src/pages/Agents.js
@@ -18,8 +18,24 @@ const Agents = () => {
           abilities: '',
           photoUrl: '',
           voiceLine: '',
-          
 
+          slot1: '', 
+          slot2: '', 
+          slot3: '', 
+          slot4: '', 
+
+          slot1name: '', 
+          slot2name: '', 
+          slot3name: '', 
+          slot4name: '',
+          
+          slot1Desc: '',
+          slot2Desc: '', 
+          slot3Desc: '', 
+          slot4Desc: '',  
+
+          description: '',
+       
         })
 
         useEffect(() => {
@@ -27,7 +43,6 @@ const Agents = () => {
             .then(res => {
               var randomNum= Math.floor(Math.random()*res.data.data.length) 
               console.log(randomNum)
-              // console.log(randomNum); 
               var CharNum = randomNum; 
               console.log(CharNum);
               
@@ -43,11 +58,35 @@ const Agents = () => {
                   + ", " + res.data.data[CharNum].abilities[2].displayName + ", and " + res.data.data[CharNum].abilities[3].displayName,
                   photoUrl: res.data.data[CharNum].bustPortrait,
                   voiceLine: res.data.data[CharNum].voiceLine.mediaList[0].wave,  
+
+                  // slot names 
+                  slot1: res.data.data[CharNum].abilities[0].slot, 
+                  slot2: res.data.data[CharNum].abilities[1].slot, 
+                  slot3: res.data.data[CharNum].abilities[2].slot, 
+                  slot4: res.data.data[CharNum].abilities[3].slot, 
+
+                  // slot names 
+                  slot1name: res.data.data[CharNum].abilities[0].displayName, 
+                  slot2name: res.data.data[CharNum].abilities[1].displayName, 
+                  slot3name: res.data.data[CharNum].abilities[2].displayName, 
+                  slot4name: res.data.data[CharNum].abilities[3].displayName,
+
+                  slot1Desc: res.data.data[CharNum].abilities[0].description, 
+                  slot2Desc: res.data.data[CharNum].abilities[1].description, 
+                  slot3Desc: res.data.data[CharNum].abilities[2].description, 
+                  slot4Desc: res.data.data[CharNum].abilities[3].description, 
+                  
+                  description: res.data.data[CharNum].description,
+
+                  
                });
 
+
+               
                console.log(Data.voiceLine);
-               console.log('gatheredvoiceline',res.data.data[CharNum].voiceLine.mediaList[0].wave)
-               setSavedVoiceLine(res.data.data[CharNum].voiceLine.mediaList[0].wave)
+               console.log('gatheredvoiceline',res.data.data[CharNum].voiceLine.mediaList[0].wave);
+               setSavedVoiceLine(res.data.data[CharNum].voiceLine.mediaList[0].wave);
+               
           
               })
 
@@ -64,37 +103,59 @@ const Agents = () => {
   
 
           
-
-          <div>
+//This is the big container - the 'one parent' 
+          <div> 
             
-            {/* <button type="randomize" onclick=  >Click me!</button> */}
-            <h1> Name: {Data.name}</h1>
-            <h1> Role: {Data.role}</h1>
-            <img src={Data.photoUrl} width = "300" height = "280" alt={Data.name} />
-            
-            <audio controls>
-              {savedVoiceLine && (
-                <source src= {savedVoiceLine} />  
-              )}
+            <h1> {Data.name}</h1>
+            <h1> {Data.role}</h1>
+             
+            <div class="container">
+              <div class="pcontainer">
+                  <p>{Data.description}</p>
+              </div>
+              
 
-            </audio>
-
-            <h1>Abilities: {Data.abilities}</h1> 
+              <img 
+              src={Data.photoUrl} 
+              width = "450" 
+              height = "450" 
+              alt={Data.name} />
+              
+              <div class="abilcontainer"> 
+                <h1>Abilities:</h1> 
+              {/* all of the names of the abilities:  */}
+              <div>
+                <span style={{ fontWeight:'bold'}}> {Data.slot1name + ": "}</span>
+                {Data.slot1Desc}
+              </div>
+              <div>
+                <span style={{ fontWeight:'bold'}}> {Data.slot2name + ": "}</span>
+                {Data.slot2Desc}
+              </div>
+              <div>
+                <span style={{ fontWeight:'bold'}}> {Data.slot3name + ": "}</span>
+                {Data.slot3Desc}
+              </div>
+              <div>
+                <span style={{ fontWeight:'bold'}}> {Data.slot4name + ": "}</span>
+                {Data.slot4Desc}
+              </div>
+                
+              </div>
+            </div>
           
-            
-      
+            <audio controls>
+                {savedVoiceLine && (
+                  <source src= {savedVoiceLine} />  
+                )}
+            </audio>
           </div>
         );
 
-        console.log(Data); 
 
       } 
 
      
-    
-    
-    
-
 
 
 export default Agents; 
